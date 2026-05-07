265cb479ce13dbea758712a7bc5d1b9856558e22
mbenic <mbenic@hotmail.com>
2026-05-07 22:16:02 +0800

    changed API_URL constant

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
-const API_URL = "http://127.0.0.1:8000/api";
+// const API_URL = "http://127.0.0.1:8000/api";
+
+const API_URL = "https://devconnect-backend-heroku-548e9ac671d6.herokuapp.com/api";
 
 export { API_URL };
